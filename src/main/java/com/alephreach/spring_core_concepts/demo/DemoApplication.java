769d7a22288fffd98d3449cfa3231b2b2b293233
wingsuncheung <brendon.cheung@outlook.com>
2019-05-31 00:51:21 -0400

    Initial commit

---
+++ src/main/java/com/alephreach/spring_core_concepts/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.alephreach.spring_core_concepts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
